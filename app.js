560c7be7f6fe6a4b232b9c91f9bc480334d532aa
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-04-13 12:25:35 -0500

    1 file modified

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const backgroundColors = [
   'Yellow',
   'Pink',
   'rgba(133,122,200)',
-  'linear-gradient(315deg, #20bf55 0%, #01baef 74%)',
+  'lightgreen',
   '#f15025',
 ];
 
@@ -14,3 +14,11 @@ const backgroundColors = [
 // getting the btn id from the index.html
 const btn = document.getElementById('btn');
 const color = document.getElementsByClassName('color');
+
+// adding a event listener to our btn id button
+btn.addEventListener('click', function () {
+  // I want to get a number between 0 & 6 with math.random
+  const randomColorNumber = ;
+  // changing the body's background
+  document.body.style.backgroundColor = backgroundColors[randomColorNumber];
+});
