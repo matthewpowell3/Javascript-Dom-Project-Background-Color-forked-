48d16d651f7cccf07e15ed84deabef112ba10d19
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-04-13 12:11:31 -0500

    2 files modified

---
+++ app.js
@@ -0,0 +1,15 @@
+// backgroundColors array
+
+const backgroundColors = [
+  'green',
+  'Yellow',
+  'Pink',
+  'rgba(133,122,200)',
+  'linear-gradient(315deg, #20bf55 0%, #01baef 74%)',
+  '#f15025',
+];
+
+// variables for the HTML content
+
+// getting the btn id from the index.html
+const btn = document.getElementById(btn);
