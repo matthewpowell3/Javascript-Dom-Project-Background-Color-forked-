468f84e3c73268bf130ce41c005577270d69f776
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-04-13 15:58:12 -0500

    1 file modified, 1 added

---
+++ hex.js
@@ -0,0 +1,37 @@
+const hex = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+];
+
+const colorSelector = document.getElementById('btn');
+const colorText = document.querySelector('.color');
+
+colorSelector.addEventListener('click', function () {
+  let hexCode = '#';
+  // use loop to loop there hexCode
+  // looping there each hexcode
+  for (let i = 0; i < 6; i++) {
+    // adding hashtag to haxcode
+    hexCode += hex[randomHexCode()];
+  }
+  colorText.textContent = hexCode;
+  document.body.style.backgroundColor = hexCode;
+});
+
+function randomHexCode() {
+  return Math.floor(Math.random() * hex.length);
+}
